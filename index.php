d618d0d6a8512bc496a54d93393259d532b8888f
Mafuyuk <inverter186@gmail.com>
2025-11-20 09:44:24 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo"Helloworld!";
+    echo"<br>";
+    echo"<h2>My name is Krittima Tinjan.</h2>";
+    ?>
+</body>
+</html>
